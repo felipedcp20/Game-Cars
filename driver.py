c47d6430840f6c573e9a00e6df72800b7022d063
felipe <felipedcp@gmail.com>
2021-08-13 10:36:14 -0500

    creando logica para solicitar la cantidad de jugadores/conductores

---
+++ driver.py
@@ -0,0 +1,10 @@
+class Driver:
+
+    def __init__ (self,name):
+        self.name = name
+
+    def printdriver (self,otherdriver):
+        print(self.name)
+
+        
+
--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+players = []
+car = []
 
 
 def run ():
-    pass
+    
+     print("""Bienbenido al juego de carreras.
+    ESCOGE UNA OPCION: 
+
+    1) Ingresa la cantidad de jugadores
+    2) Ingresa la distancia de la pista
+    3) A jugar
+    4) Imprimir podio: """)
+
+     opcion = int(input())
+     if opcion == 1:
+
+         print("ingresa la cantidad: ")
+         countplayers = int(input())
+         count = 1
+
+         while(countplayers != 0):
+             name = input(f'ingresa el nombre del conductor # {count}: ')
+             
+             count +=1
+             countplayers -=1
+
+                
+
+     elif opcion == 2:
+        pass
+     elif opcion == 3:
+         pass
+     elif opcion ==4:
+         pass
+     else:
+         print("escoge una opcion correcta")
+    
 
 if __name__ == '__main__':
     run()
