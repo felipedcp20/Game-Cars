1ef09f995c8450010433c590d42dc885533718ec
felipe <felipedcp@gmail.com>
2021-08-13 09:10:04 -0500

    creando Objetos y metodos

---
+++ carro.py
@@ -0,0 +1,5 @@
+class Carro:
+    
+    def __init__(self,conductor):
+        pass
+    
---
+++ pista.py
@@ -0,0 +1,8 @@
+class Pista:
+
+    def __init__(self):
+        pass
+        
+
+    def carril (carro):
+        pass
